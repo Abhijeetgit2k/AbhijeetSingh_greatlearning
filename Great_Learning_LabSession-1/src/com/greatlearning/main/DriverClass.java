912635c905582f0255fd3218bffc87396b1af4c7
Abhijeetgit2k <119854368+Abhijeetgit2k@users.noreply.github.com>
2022-12-05 16:10:51 +0530

    Add files via upload

---
+++ Great_Learning_LabSession-1/src/com/greatlearning/main/DriverClass.java
@@ -0,0 +1,74 @@
+package com.greatlearning.main;
+
+import java.util.Scanner;
+import com.greatlearning.interfaces.ICredentials;
+import com.greatlearning.model.Employee;
+import com.greatlearning.service.CredentialService;
+
+public class DriverClass {
+
+	public static void main(String[] args) {
+		
+		Employee employee = new Employee("Harshit", "Chaudhary");
+		
+		ICredentials cs = new CredentialService();
+		
+		System.out.println("Please enter the Deparment from the following");
+		System.out.println("1. Techinical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+
+		Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+		
+		String generatedEmail = null;
+		String generatedPassword = null;
+		
+			
+		
+		switch (option) {
+		
+		case 1:{
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "tech");
+			generatedPassword = cs.generatePassword();
+			break;
+		}
+		
+		case 2:{
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "adm");
+			generatedPassword = cs.generatePassword();
+			break;
+		}
+		
+		case 3:{
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "hr");
+			generatedPassword = cs.generatePassword();
+			break;
+		}
+		
+		case 4:{
+			generatedEmail = cs.generateEmailAddress(employee.getFirstName().toLowerCase(),
+					employee.getLastName().toLowerCase(), "lgl");
+			generatedPassword = cs.generatePassword();
+			break;
+		}
+		
+		default: {
+			
+			System.out.println("Invalid choice");
+		}
+		
+		}
+	
+		
+		employee.setEmail(generatedEmail);
+		employee.setPassword(generatedPassword);
+		cs.showCredentials(employee);
+		sc.close();
+	}
+
+}
